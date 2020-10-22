b0c7e9e057082b33b8b00fbfddcc0be321d28b2f
Shilparpatil <49277312+Shilparpatil@users.noreply.github.com>
2020-10-22 14:35:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# college-voting-system
+Effective beginning September 10, 2017, the Select2 documentation repository is now available at [`select2/docs`](https://github.com/select2/docs).
---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; brought to you by Rukmini T & Shilpa </strong>
+    </div>
+    <!-- /.container -->
+</footer>
